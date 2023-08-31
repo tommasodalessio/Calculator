40d76b1829594874325de957ea9b6a2095082fee
tommasodalessio <117944144+tommasodalessio@users.noreply.github.com>
2023-08-31 22:30:49 +0200

    function operation

--- script.js
+++ script.js
@@ -7,26 +7,38 @@ we need function for:
     -operation: a function that takes two parameter and an operator a do the math recalling the above function
 */
 
-function add (addend1, addend2) {
-    addend1 = Number(addend1);
-    addend2 = Number(addend2);
-    return addend1 + addend2;
+function add (operator1, operator2) {
+    operator1 = Number(operator1);
+    operator2 = Number(operator2);
+    return operator1 + operator2;
 }
 
-function subtract (addend1, addend2) {
-    addend1 = Number(addend1);
-    addend2 = Number(addend2);
-    return addend1 - addend2;
+function subtract (operator1, operator2) {
+    operator1 = Number(operator1);
+    operator2 = Number(operator2);
+    return operator1 - operator2;
 }
 
-function multiply (addend1, addend2) {
-    addend1 = Number(addend1);
-    addend2 = Number(addend2);
-    return addend1 * addend2;
+function multiply (operator1, operator2) {
+    operator1 = Number(operator1);
+    operator2 = Number(operator2);
+    return operator1 * operator2;
 }
 
-function divide (addend1, addend2) {
-    addend1 = Number(addend1);
-    addend2 = Number(addend2);
-    return addend1 / addend2;
+function divide (operator1, operator2) {
+    operator1 = Number(operator1);
+    operator2 = Number(operator2);
+    return operator1 / operator2;
 }
+
+function operation (operator1, operator2, sign) {
+    if(sign == "plus") return add(operator1, operator2);
+    if(sign == "minus") return subtract(operator1, operator2);
+    if(sign == "times") return multiply(operator1, operator2);
+    if(sign == "divide") return divide(operator1, operator2);
+}
+
+console.log(operation(5, 5, "plus"));
+console.log(operation(5, 5, "minus"));
+console.log(operation(5, 5, "times"));
+console.log(operation(5, 5, "divide"));
